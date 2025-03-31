3e1d056a4f0305b971d932f63ad6bc90d25245f6
Barka <unlimitediq@proton.me>
2025-03-31 16:04:43 +0200

    Revert "Set metadata"

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,4 +1,5 @@
-import { Settings as SettingsIcon } from "lucide-react";
+import { useState } from "react";
+import { Minus, Plus } from "lucide-react";
 import { Button } from "./ui/button";
 import {
   Drawer,
@@ -8,35 +9,38 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "./ui/drawer";
-import { PomodoroStage } from "@/utils/time";
-import { TimeSelector } from "./TimeSelector";
-import { useSettings } from "@/hooks/useSettings";
-import { StageSelector } from "./StageSelector";
-
+import { formatTime, DEFAULT_TIMES } from "@/utils/time";
 
 interface SettingsProps {
-  setStage: (stage: PomodoroStage) => void;
-  currentStage: PomodoroStage;
-  className?: string;
+  setTime: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const Settings = ({ setStage, currentStage }: SettingsProps) => {
-  const {
-    selectedTime,
-    selectedStage,
-    open,
-    setOpen,
-    handleTimeChange,
-    handleStageChange,
-    handleClose,
-  } = useSettings({ currentStage, onStageChange: setStage });
+const Settings = ({ setTime }: SettingsProps) => {
+  const [mode, setMode] = useState<keyof typeof DEFAULT_TIMES>("work");
+  const [selectedTime, setSelectedTime] = useState<number>(DEFAULT_TIMES[mode]);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const onClick = (value: number) => {
+    setSelectedTime((prev) => prev + value);
+  };
+
+  const handleModeChange = (newMode: "work" | "pause" | "break") => {
+    setMode(newMode);
+    const newTime = DEFAULT_TIMES[newMode];
+    setSelectedTime(newTime);
+  };
+
+  const onClose = (isValidated: boolean) => {
+    if (isValidated) {
+      setTime(selectedTime);
+    }
+    setOpen(false);
+  };
 
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger>
-        <Button variant="ghost">
-          <SettingsIcon />
-        </Button>
+        <Button>open</Button>
       </DrawerTrigger>
       <DrawerContent>
         <div className="mx-auto w-full max-w-sm py-4">
@@ -44,23 +48,64 @@ const Settings = ({ setStage, currentStage }: SettingsProps) => {
             <DrawerTitle>Réglage du pomodoro</DrawerTitle>
             <DrawerDescription>Réglez la durée du pomodoro</DrawerDescription>
           </DrawerHeader>
+          <div className="flex items-center space-y-4">
+            <Button
+              variant="outline"
+              size="icon"
+              className="h-8 w-8 shrink-0 rounded-full"
+              onClick={() => onClick(-10)}
+              disabled={selectedTime <= 0}
+            >
+              <Minus />
+            </Button>
+            <div className="flex-1 text-center">
+              <div className="text-7xl font-bold tracking-tighter">
+                {formatTime(selectedTime)}
+              </div>
+            </div>
+            <Button
+              variant="outline"
+              size="icon"
+              className="h-8 w-8 shrink-0 rounded-full"
+              onClick={() => onClick(10)}
+              disabled={selectedTime >= 60 * 60}
+            >
+              <Plus />
+            </Button>
+          </div>
 
-          <TimeSelector
-            selectedTime={selectedTime}
-            onTimeChange={handleTimeChange}
-          />
-
-          <StageSelector
-            selectedStage={selectedStage}
-            onStageChange={handleStageChange}
-          />
-
-
+          <div className="flex space-x-4 mt-4">
+            {[
+              { id: "work", label: "Work", icon: "🔥" },
+              { id: "pause", label: "Pause", icon: "☕" },
+              { id: "break", label: "Break", icon: "😴" },
+            ].map((period) => (
+              <div key={period.id} className="flex flex-1">
+                <input
+                  type="radio"
+                  name="period"
+                  id={period.id}
+                  checked={mode === period.id}
+                  onChange={() =>
+                    handleModeChange(period.id as keyof typeof DEFAULT_TIMES)
+                  }
+                  className="hidden peer"
+                />
+                <label
+                  htmlFor={period.id}
+                  className="flex flex-1 flex-col items-center rounded-lg peer bg-neutral-400 text-black px-4 peer-checked:bg-rose-500 peer-checked:text-white"
+                >
+                  <div>{period.label}</div>
+                  <div>{period.icon}</div>
+                </label>
+              </div>
+            ))}
+          </div>
           <div className="flex mt-4 space-x-4">
-            <Button variant="outline" onClick={() => handleClose(false)}>
+            <Button variant={"outline"} onClick={() => onClose(false)}>
               Annuler
             </Button>
-            <Button className="flex-1" onClick={() => handleClose(true)}>
+            <Button className="flex-1" onClick={() => onClose(true)}>
               Appliquer
             </Button>
           </div>
--- src/config/periods.ts
+++
@@ -1,7 +0,0 @@
-import { PomodoroStage } from "@/utils/time";
-
-export const periods: { id: PomodoroStage; label: string; icon: string }[] = [
-  { id: "work", label: "Work", icon: "🔥" },
-  { id: "shortBreak", label: "Pause", icon: "☕" },
-  { id: "longBreak", label: "Break", icon: "😴" },
-]; 
